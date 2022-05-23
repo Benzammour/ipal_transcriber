fb4e0b85e4223d794506bd4786daaf8ff0fcee00
Samir Benzammour <samir.benzammour@gmail.com>
2022-05-23 20:50:53 +0200

    chore: cleans code; adds comments; adds state_identifier

--- transcribers/mqtt.py
+++ transcribers/mqtt.py
@@ -26,6 +26,16 @@ class MqttTranscriber(Transcriber):
         14: Activity.COMMAND
     }
 
+    @classmethod
+    def state_identifier(cls, msg, key):
+        if msg.activity in [str(Activity.INTERROGATE), str(Activity.COMMAND)]:
+            return "{}:{}".format(msg.dest, key)
+        elif msg.activity in [str(Activity.INFORM), str(Activity.ACTION)]:
+            return "{}:{}".format(msg.src, key)
+        else:
+            settings.logger.critical("Unknown activity {}".format(msg.activity))
+            return "{}:{}".format(msg.src, key)
+
     def matches_protocol(self, pkt):
         return "MQTT" in pkt
 
@@ -34,7 +44,6 @@ class MqttTranscriber(Transcriber):
         dest = "{}:{}".format(pkt["IP"].dst, pkt["TCP"].dstport)
 
         res = []
-        pkt_offset = 0
         for mqtt_pkt in pkt.get_multiple_layers("MQTT"):
 
             next_id = self._id_counter.get_next_id()
@@ -82,7 +91,6 @@ class MqttTranscriber(Transcriber):
             new_msg._match_to_requests = msg_type in [2, 4, 5, 6, 7, 9, 11, 13]
 
             res.append(new_msg)
-            pkt_offset += length
 
         return res
 
@@ -93,23 +101,26 @@ class MqttTranscriber(Transcriber):
                 response.responds_to = [ipal_pkt.id for ipal_pkt in requests if ipal_pkt.type == 1]
                 return [ipal_pkt for ipal_pkt in requests if ipal_pkt.type == 1]
 
-            case 4 | 5 | 6 | 7 | 9 | 11: # PubACK, PubREC, PubREL, PubCOMP, SubACK, UnsubACK
+            case 4 | 5 | 6 | 7 | 9 | 11: # MQTT QoS 1 & 2
                 res_to_type = [0, 0, 0, 0, 3, 3, 5, 6, 0, 8, 0, 10][response.type]
 
-                responds_to_packets = [ipal_pkt for ipal_pkt in requests if ipal_pkt.type == res_to_type 
-                    and ipal_pkt._mqtt_msg_id == response._mqtt_msg_id 
-                    and ipal_pkt.src == response.dest
-                    and ipal_pkt.dest == response.src]
+                # every packet that responds to the ACK
+                responds_to_packets = [ipal_pkt for ipal_pkt in requests 
+                    if ipal_pkt.type == res_to_type 
+                        and ipal_pkt._mqtt_msg_id == response._mqtt_msg_id 
+                        and ipal_pkt.src == response.dest
+                        and ipal_pkt.dest == response.src]
 
+                # every id for each packet from above
                 response.responds_to = [packet.id for packet in responds_to_packets]
 
-                if len(response.responds_to) == 0:
-                    settings.logger.critical("Found no request for ACK!")
+                # make sure every ACK has a request
+                assert len(response.responds_to) != 0, settings.logger.critical("Found no request for ACK!")
 
-                # Publish ACK
-                if response.type == 4:
+                # MQTT QoS 1 & 2
+                if response.type == 4:  # PubACK
                     return responds_to_packets
-                elif response.type in [7, 9, 11]: 
+                elif response.type in [7, 9, 11]:   # PubREC, PubREL, PubCOMP, SubACK, UnsubACK
                     return [ipal_pkt for ipal_pkt in requests
                         if ipal_pkt._mqtt_msg_id == response._mqtt_msg_id 
                             and (ipal_pkt.src == response.dest or ipal_pkt.src == response.src)
@@ -138,7 +149,6 @@ class MqttTranscriber(Transcriber):
 
     @staticmethod
     def __parse_mqtt_pkt_id(mqtt_pkt):
-        # print(int(mqtt_pkt.msgtype))
         match int(mqtt_pkt.msgtype):
             case 1 | 2 | 12 | 13 | 14: # Connect, ConnACK, PingREQ, PingRESP, Disconnect
                 return None
@@ -148,7 +158,5 @@ class MqttTranscriber(Transcriber):
                     return mqtt_pkt.msgid
                 return None
 
-            
-                
             case 4 | 5 | 6 | 7| 8 | 9 | 10 | 11: # PubACK, PubREC, PubREL, PubCOMP, Subscribe, SubACK, Unsub, UnsubACK
                 return mqtt_pkt.msgid
